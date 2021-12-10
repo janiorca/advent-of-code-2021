8c7980a551ed006706b4980f8fded0ee2066ebff
SLIMPANDA\janio <jani.peltonen@gmail.com>
2021-12-10 09:06:21 +0000

    aoc8

--- src/bin/aoc5.rs
+++ src/bin/aoc5.rs
@@ -2,9 +2,26 @@ use std::fs;
 
 fn main() {
     // Part 1
-    let lines = fs::read_to_string("inputs/test.txt").unwrap().lines();
-    for line in lines{
-        let nums: Vec<u32> = line.split( |x|" -> ").;
+    let input = fs::read_to_string("inputs/test.txt").unwrap();
+    let t = 
+    let mut sea_map = Box::new( vec![vec![1024;0]0]); 
+    for line in input.lines(){
+        let coords: Vec<u32> = line.replace(" -> ", ",").split(',').map(|v|u32::from_str_radix( v, 10 ).unwrap() ).collect();
+        if coords[0] == coords[2] { // Same X
+            for y in coords[1]..=coords[3] {
+                sea_map[ y as usize ][ coords[0] as usize] += 1;
+            }
+        }
+        if coords[1] == coords[3] { // Same Y
+            for x in coords[0]..=coords[2] {
+                sea_map[ coords[1] as usize ][ x as usize] += 1;
+            }
+        }
     }
-    let mut fishes: Vec<i32> = input.split(',').map(|x|i32::from_str_radix(x, 10).unwrap()).collect();
+    // Count map locations with value >= 2
+    let mut count = 0;
+    for row in &sea_map..iter() {
+        count += row.iter().filter(|x|**x>=2).count();
+    }
+    print!( "dangerous positions {}", count);
 }
--- src/bin/aoc8.rs
+++ src/bin/aoc8.rs
@@ -16,15 +16,20 @@ fn main() {
     }
     println!( "1,4,7 or 8 appears {} times in results", easy_counts );
 
-    let input_file = fs::read_to_string("inputs/test.txt").unwrap();
+    // Part 2
+    // a over conplicated solution that uses the number of times the the individual segments appear to figure out which segments
+    // map to digits.... Good excercise with using Rust sets...
+    let mut total = 0;
+    let input_file = fs::read_to_string("inputs/aoc8.txt").unwrap();
     for line in input_file.lines() {
         let parts: Vec<&str> = line.split("|").collect();
         let inputs: Vec<&str> = parts[ 0 ].split_whitespace().collect();
         let mut segment_counts: BTreeMap<char,u32> = BTreeMap::new();
         let mut segments_to_digit: BTreeMap<BTreeSet<char>, u32> = BTreeMap::new();
-        let mut unresolved_segments: Vec<BTreeSet<char>> = Vec::new();
+        let mut segments_5: Vec<BTreeSet<char>> = Vec::new();
+        let mut segments_6: Vec<BTreeSet<char>> = Vec::new();
         for part in inputs {
-            let mut segments: BTreeSet<char> = part.as_bytes().iter().map(|x|*x as char).collect();
+            let segments: BTreeSet<char> = part.as_bytes().iter().map(|x|*x as char).collect();
             for segment in &segments {
                 *segment_counts.entry(*segment).or_insert(0)+=1;
             }
@@ -33,17 +38,51 @@ fn main() {
                 4 => { segments_to_digit.insert(segments, 4 ); },
                 3 => { segments_to_digit.insert(segments, 7 ); },
                 7 => { segments_to_digit.insert(segments, 8 ); },
-                _ => { unresolved_segments.push(segments);}
+                5 => { segments_5.push(segments); },
+                6 => { segments_6.push(segments); },
+                _ => { panic!("Unexpected segment combo"); }
             }
         }
-        // Find segment F used in nine digits
+        // Find segment F (only one used in nine )digits
         let segment_F = segment_counts.iter().find(|(key,val)|**val==9).unwrap().0;
-        // the one not containing it is 2
-        let ( digit_2, unresolved_segments ):(Vec<BTreeSet<char>>, Vec<BTreeSet<char>> ) = unresolved_segments.iter().partition_in_place(|segs|!segs.contains(segment_F));
+        // the 5-segmenter containing it is 2
+        let number_2 = segments_5.iter().position(|segs| !segs.contains(segment_F)).unwrap();
+        segments_to_digit.insert(segments_5.remove(number_2), 2 );
 
+        // The segment B is the only one used by six dights
+        let segment_B = segment_counts.iter().find(|(key,val)|**val==6).unwrap().0;
+        // the five segmenter containing it is 5
+        let number_5 = segments_5.iter().position(|segs| segs.contains(segment_B)).unwrap();
+        segments_to_digit.insert(segments_5.remove(number_5), 5 );
+        // the one the doesnt is 3
+        segments_to_digit.insert(segments_5.remove(0), 3 );
 
-        let t = 0;
+        // The segment E is the only one used by four dights
+        let segment_E = segment_counts.iter().find(|(key,val)|**val==4).unwrap().0;
+        // 9 is the only 6 segmenter that doesnt use it
+        let number_9 = segments_6.iter().position(|segs| !segs.contains(segment_E)).unwrap();
+        segments_to_digit.insert(segments_6.remove(number_9), 9 );
+        
+        let seg8s = segments_6[0].iter().filter(|c|*segment_counts.get(*c).unwrap() == 8).count();
+        if seg8s == 2  {
+            segments_to_digit.insert(segments_6.remove(0), 0 );
+            segments_to_digit.insert(segments_6.remove(0), 6 );
+        } else {
+            segments_to_digit.insert(segments_6.remove(0), 6 );
+            segments_to_digit.insert(segments_6.remove(0), 0 );
+        }
 
+        // We have a map, now convert the output with it
+        let outputs: Vec<&str> = parts[ 1 ].split_whitespace().collect();
+        let mut value = 0;
+        for part in outputs {
+            value *= 10;
+            let segments: BTreeSet<char> = part.as_bytes().iter().map(|x|*x as char).collect();
+            let digit = segments_to_digit.get(&segments).unwrap();
+            value += digit;
+        }
+        total += value;
     }
+    println!("the total is {}", total);
  
 }
